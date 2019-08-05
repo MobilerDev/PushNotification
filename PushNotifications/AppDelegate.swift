8504080af9db5599f467d5dafc8f6584f4b24df3
Mustafa Onur Ilgaz <MustafaOnur.Ilgaz@ykteknoloji.com.tr>
2019-08-05 13:43:05 +0300

    First commit

--- PushNotifications/AppDelegate.swift
+++ PushNotifications/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
-        print("Device token: \(token)")
+        print("Device token: \(token) ")
     }
     
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
